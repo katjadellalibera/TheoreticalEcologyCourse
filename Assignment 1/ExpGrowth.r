4634cb9a3e942f98ed49fd42a94602384aeb660c
Katja Della Libera <katja.dellalibera@gmail.com>
2022-01-19 13:48:04 -0600

    Levin, Theta Logistic Numerical Integration

--- Assignment 1/ExpGrowth.r
+++ Assignment 1/ExpGrowth.r
@@ -20,7 +20,7 @@ ExponentialGrowth<-function(t, state, parameters) {
 RunExponentialGrowth<-function(MaxTime=10,GrowthRate=0.2,InitialX=1.0){
   times <- seq(0, MaxTime, by = 0.01)
   parameters <- c(r=GrowthRate)
-  state <- c(X=InitialX)
+  state <- c(ExponentialX=InitialX)
   out <- ode(y = state, times = times, func = ExponentialGrowth, parms = parameters)
   print(plot(out[,2]~out[,1],type="l",xlab="time",ylab="Density of X"))
   return(out)
@@ -28,14 +28,7 @@ RunExponentialGrowth<-function(MaxTime=10,GrowthRate=0.2,InitialX=1.0){
 
 # run the solver for exponential growth 
 out <- RunExponentialGrowth()
-# plot the solution 
-ggplot(as.data.frame(out)) +
-  aes(x=time, y=X) +
-  geom_line() +
-  ggtitle("Exponential growth") +
-  ylab("Density of X") +
-  xlab("Time") + 
-  theme_bw()
+
 
 
 ### Logistic Growth ###
@@ -55,7 +48,7 @@ RunLogisticGrowth<-function(MaxTime = 10, GrowthRate = 0.5,
                             CarryingCapacity = 5, InitialX = 1.0){
   times <- seq(0, MaxTime, by = 0.01)
   parameters <- c(r=GrowthRate,k=CarryingCapacity)
-  state <- c(X=InitialX)
+  state <- c(LogisticX=InitialX)
   out <- ode(y = state, times = times, func = LogisticGrowth, parms = parameters)
   print(plot(out[,2]~out[,1],type="l",xlab="time",ylab="Density of X"))
   return(out)
@@ -63,14 +56,7 @@ RunLogisticGrowth<-function(MaxTime = 10, GrowthRate = 0.5,
 
 # run the solver for exponential growth 
 logisticOut <- RunLogisticGrowth()
-# plot the solution 
-ggplot(as.data.frame(logisticOut)) +
-  aes(x=time, y=X) +
-  geom_line() +
-  ggtitle("Logistic growth") +
-  ylab("Density of X") +
-  xlab("Time") + 
-  theme_bw()
+
 
 
 ### Allee Growth ###
@@ -91,7 +77,7 @@ RunAlleeGrowth<-function(MaxTime = 10, UpperGrowthRate = 2,
                           CriticalPoint = 5, DeathRate = 0.1, InitialX = 1.0){
   times <- seq(0, MaxTime, by = 0.01)
   parameters <- c(u=UpperGrowthRate,v=CriticalPoint, c= DeathRate)
-  state <- c(X=InitialX)
+  state <- c(AlleeX=InitialX)
   out <- ode(y = state, times = times, func = AlleeGrowth, parms = parameters)
   print(plot(out[,2]~out[,1],type="l",xlab="time",ylab="Density of X"))
   return(out)
@@ -99,48 +85,78 @@ RunAlleeGrowth<-function(MaxTime = 10, UpperGrowthRate = 2,
 
 # run the solver for exponential growth 
 AlleeOut <- RunAlleeGrowth()
-# plot the solution 
-ggplot(as.data.frame(AlleeOut)) +
-  aes(x=time, y=X) +
-  geom_line() +
-  ggtitle("Allee growth") +
-  ylab("Density of X") +
-  xlab("Time") + 
-  theme_bw()
 
 
-### Allee Growth ###
+
+### Levin Growth ###
 
 ## This function takes a step in time
-AlleeGrowth<-function(t, state, parameters) { 
+LevinGrowth<-function(t, state, parameters) { 
   X <- state[1] ## the first element is the density of X at time t
-  u <- parameters[1] ## the first parameter is the upper limit of the growth rate
-  v <- parameters[2] ## the second parameter is critical point
-  c <- parameters[3] ## the third parameter is the density dependent death rate
-  dX<-((u*X/(v+X)-c*X)*X)
+  c <- parameters[1] ## the first parameter is the colonization rate
+  e <- parameters[2] ## the second parameter is the extinction
+  dX<-(c*X*(1-X)-e*X)
   return(list(dX)) ## for some reason, you have to return a list
 }
 
 
 ## This function runs the model and produces the trajectory
-RunAlleeGrowth<-function(MaxTime = 10, UpperGrowthRate = 2,
-                         CriticalPoint = 5, DeathRate = 0.1, InitialX = 1.0){
+RunLevinGrowth<-function(MaxTime = 10, ColonizationRate = 0.7,
+                          ExtinctionRate = 0.1, InitialX = 0.1){
   times <- seq(0, MaxTime, by = 0.01)
-  parameters <- c(u=UpperGrowthRate,v=CriticalPoint, c= DeathRate)
-  state <- c(X=InitialX)
-  out <- ode(y = state, times = times, func = AlleeGrowth, parms = parameters)
+  parameters <- c(c= ColonizationRate, e= ExtinctionRate)
+  state <- c(LevinX=InitialX)
+  out <- ode(y = state, times = times, func = LevinGrowth, parms = parameters)
   print(plot(out[,2]~out[,1],type="l",xlab="time",ylab="Density of X"))
   return(out)
 }
 
 # run the solver for exponential growth 
-AlleeOut <- RunAlleeGrowth()
-# plot the solution 
-ggplot(as.data.frame(AlleeOut)) +
-  aes(x=time, y=X) +
+LevinOut <- RunLevinGrowth()
+
+
+### Theta-Logistic Growth ###
+
+## This function takes a step in time
+ThetaLogisticGrowth<-function(t, state, parameters) { 
+  X <- state[1] ## the first element is the density of X at time t
+  r <- parameters[1] ## the first parameter is the growth rate
+  k <- parameters[2] ## the second parameter is the carrying capacity
+  theta <- parameters[3] ## the third parameter is the theta
+  dX<-r*X*(1-(X/k)^theta)
+  return(list(dX)) ## for some reason, you have to return a list
+}
+
+
+## This function runs the model and produces the trajectory
+RunThetaLogisticGrowth<-function(MaxTime = 10, GrowthRate = 1,
+                         CarryingCapacity = 10, Theta = 1,
+                         InitialX = 1){
+  times <- seq(0, MaxTime, by = 0.01)
+  parameters <- c(r= GrowthRate, k = CarryingCapacity, theta = Theta)
+  state <- c(ThetaX=InitialX)
+  out <- ode(y = state, times = times, func = ThetaLogisticGrowth, parms = parameters)
+  print(plot(out[,2]~out[,1],type="l",xlab="time",ylab="Density of X"))
+  return(out)
+}
+
+# run the solver for exponential growth 
+ThetaLogisticOut <- RunThetaLogisticGrowth()
+
+
+
+### Combine into one plot
+allOut <- merge(as.data.frame(out),as.data.frame(logisticOut), 
+                by = "time")
+allOut <- merge(allOut,as.data.frame(AlleeOut), by = "time")
+allOut <- merge(allOut,as.data.frame(LevinOut), by = "time")
+allOut <- merge(allOut,as.data.frame(ThetaLogisticOut), by = "time")
+
+
+ggplot(as.data.frame(ThetaLogisticOut)) +
+  aes(x=time, y=ThetaX) +
   geom_line() +
-  ggtitle("Allee growth") +
+  ggtitle("Theta-Logistic growth") +
   ylab("Density of X") +
   xlab("Time") + 
   theme_bw()
-
