334636eba2834b878d72ef9ea8ebe16e5804fcb4
Katja Della Libera <katja.dellalibera@gmail.com>
2022-01-30 15:40:08 -0600

    draft question 1

--- Assignment 3/Community_matrices.r
+++ Assignment 3/Community_matrices.r
@@ -2,6 +2,11 @@
 ## Stefano Allesina and Sarah Cobey 
 ## Code for Theoretical Ecology
 
+library(ggplot2)
+library(psych)
+S <- 250
+sigma <- 0.3
+
 MayMatrix<-function(S,C,sigma){
   ## This matrix determines the connections
   A<-matrix(runif(S*S),S,S)
@@ -28,3 +33,30 @@ PPMatrix<-function(S,C,sigma){
   diag(M)<- -1
   return(M)
 }
+
+stableQ<- function(m){
+  if(tr(m)<0 & det(m)>0){
+    return(1)
+  }else{
+    return(0)
+  }
+}
+
+
+C <-seq(0.01,0.5,by=0.01)
+
+mays <- c()
+for (cs in C){
+  mays <- append(mays,sum(replicate(1000, stableQ(MayMatrix(S,cs,sigma)), simplify = TRUE )))
+}
+     
+pred <- c()
+for (cs in C){
+  pred <- append(pred,sum(replicate(1000, stableQ(PPMatrix(S,cs,sigma)), simplify = TRUE )))
+}
+
+data <- data.frame(C,mays,pred)
+
+ggplot(data, aes(x = C)) +
+  geom_line(aes(y = mays), color = "black") +
+  geom_line(aes( y = pred), color = "red")
